d8db808cfeea97d8a863c9f9f3cac1ba0ee54bae
Evandro Neto <evandro.rsneto@gmail.com>
2026-02-24 16:41:28 -0300

    Replace Gunicorn command in Procfile with a script to handle static files and migrations

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+echo "Running collectstatic..."
+cd src
+python manage.py collectstatic --noinput -i tailwind.css
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Starting Gunicorn..."
+exec gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
